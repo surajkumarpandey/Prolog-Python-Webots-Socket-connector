60e743b8bb9f9a2cb9ba9ad252e1475854cd96e6
surajkumarpandey <pandey.suraj3@gmail.com>
2020-09-19 13:40:45 +0530

    Create Webots_Prolog_client.pl
    Prolog client created to control e-puck robot in webots. Prolog client gets the sensor values from python server in webots and then returns with the appropriate actuation command.

---
+++ Webots_Prolog_client.pl
@@ -0,0 +1,40 @@
+
+
+
+:- use_module(library(socket)).
+:- use_module(library(streampool)).
+
+
+init	:-	tcp_socket(Socket),
+                tcp_connect(Socket, localhost:9999),
+                tcp_open_socket(Socket, In, Out),
+		chat_to_server(In, Out),
+                close_connection(In, Out),
+		tcp_close_socket(Socket).
+		
+
+chat_to_server(In, Out) :-                          
+	read_string(In, "]", "", End,X),        
+	split_string(X, ",", "", List),
+	nth0(0, List, Ps0),
+        nth0(7, List, Ps7),
+	number_string(P0, Ps0),
+	number_string(P7, Ps7),	
+	writeln(P0),writeln(P7),
+	((P0 > 80 ; P7 > 80) ->
+		reply(Out,l), writeln("left turn");
+		reply(Out,f),writeln("Straight")),
+	chat_to_server(In, Out).
+
+chat_to_server(_In, _Out) :-
+        print_message(warning,'chat failed'),!.
+
+reply(Out,Direction):-
+	format(Out,'~q',Direction),                  
+        flush_output(Out).
+	
+
+    
+close_connection(In, Out) :-
+        close(In, [force(true)]),
+        close(Out, [force(true)]).
